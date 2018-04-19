0be3df01ceafa7e25425538a6cf41db3bc0549f4
Espen Haabeth <haabethespen@gmail.com>
2018-04-19 14:15:24 +0200

    items, objects, shop, board, storage
    jeg er god

--- load.js
+++ load.js
@@ -1,11 +1,50 @@
 var ASPECT_RATIO = 4/3, height, width;
 var canvas, context;
 var objects = [];
-
+var items = [];
 window.addEventListener("load", load);
 function load() {
     canvas = document.querySelector("canvas");
     context = canvas.getContext("2d");
+
+    objects.push({
+        name: "Shop", 
+        render: function() {},
+        tick: function() {},
+    }); 
+
+    objects.push({
+        name: "Board", 
+        render: function() {},
+        tick: function() {},
+    }); 
+
+    objects.push({
+        name: "Storage", 
+        render: function() {},
+        tick: function() {},
+    }); 
+
+    items.push({
+        
+        cost: [],
+        name: "Well",
+        price: 50, 
+        render: function() {},
+          
+    }); 
+
+    items.push({
+        
+        cost: [],
+        name: "Corn",
+        price: 50, 
+        render: function() {},
+          
+    }); 
+
+    
+    
     window.addEventListener("resize", resize);
     setInterval(tick, 1000/30);
     
@@ -13,3 +52,8 @@ function load() {
 
 //lag en array med data fordi bare en array for de greiene
 //var obj ["shop", ];
+//items
+//name
+//render
+//price 
+//cost: [], [], [],
