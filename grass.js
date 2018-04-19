f7442cdfc5bb6cbdeca69092e62bb95bbb1a6b5b
Espen Haabeth <haabethespen@gmail.com>
2018-04-19 21:22:44 +0200

    Moved items into new files
    prøvd å flytt dæm, ganske sikkert masse feil :P

---
+++ grass.js
@@ -0,0 +1,16 @@
+load_sequence.push(
+    function() {
+        // 1 - Grass
+        items.push({
+        click_amount: 1,
+        cost: [[0, 1]],
+        growth_amount: 4,
+        growth_time: 4,
+        name: "Tussock",
+        price: 100,
+        product: "Grass",
+        product_amount: 4,
+        product_value: 30,
+        render: function () { },
+    });
+    });
---
+++ water.js
@@ -0,0 +1,17 @@
+load_sequence.push(
+    function() {
+        // 0 - Water
+        items.push({
+        click_amount: 3,
+        cost: [],
+        growth_amount: 0,
+        growth_time: 4,
+        name: "Well",
+        price: 50,
+        product: "Water",
+        product_amount: -1,
+        product_value: 2,
+        render: function () { },
+    });
+    });
+
