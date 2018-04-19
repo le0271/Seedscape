2a0f2ff15e32171477576d85f6b6a92733e1c3f4
le0271 <leonard.lns.123@gmail.com>
2018-04-19 15:01:57 +0200

    Bug fix

--- render.js
+++ render.js
@@ -1,6 +1,6 @@
 function render() {
     context.fillStyle = "#A1887F";
-    context.fillRect(0, 0, width, height);
+    context.fillRect(0, 0, width, width / ASPECT_RATIO);
     for (var i = 0; i < objects.length; i++) {
         objects[i].render();
     }
